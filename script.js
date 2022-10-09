ba899b4e0f08a16322a9abf83d35802db5afdd54
TomyAlberdi <tomas.alberdi.2001@gmail.com>
2022-10-09 15:46:21 -0300

    Started JS

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ sw.forEach(e => {
         body.classList.toggle('dark')
     })
 })
+
+const form = document.querySelector('form')
+const input = document.querySelector('input')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    if (input.value != '' || input.value != null || input.value != undefined) {
+        crearTask(input.value)
+    }
+})
+
+const taskList = document.querySelector('.task-list')
+const cont = document.querySelector('.cont')
+
+const crearTask = (val) => {
+    let div = document.createElement('div')
+    div.classList.add('task')
+    let temp = `
+        <div class="subt">
+            <div class="check"></div>
+            <p>${val}</p>
+        </div> 
+        <img src="./images/icon-cross.svg" alt="" class="cross">
+    `
+    div.innerHTML = temp
+    taskList.insertBefore(div,cont)
+}
